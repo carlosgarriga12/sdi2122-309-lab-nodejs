6654f4b64adbc4216451ef843cfd2980fe2ded96
carlosgarriga12 <carlosgarriga12@gmail.com>
2022-03-25 10:54:27 +0100

    SDI-2122-309-7.5-Vistas y motores de plantillas.

--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -1,22 +1,25 @@
-module.exports = function(app) {
+module.exports = function(app, twig) {
     app.get("/songs", function (req, res) {
-        let response = ""
+        let songs = [{
+            "title": "Blank space",
+            "price": "1.2"
+        }, {
+            "title": "See you again",
+            "price": "1.3"
+        }, {
+            "title": "Uptown funk",
+            "price": "1.1"
+        }]
 
-        if(req.query.title != null && typeof (req.query.title) != "undefined")
-            response = 'Titulo: ' + req.query.title + '<br>'
-        if(req.query.author != null && typeof (req.query.author) != "undefined")
-            response = 'Author: ' + req.query.author + '<br>'
-
-        res.send(response);
+        let response = {
+            seller:'Tienda de canciones',
+            songs: songs
+        };
+        res.render("shop.twig", response);
     });
     app.get('/add', function(req, res) {
         let response = parseInt(req.query.num1) + parseInt(req.query.num2);
         res.send(String(response));
-    });
-
-    app.get('/songs/:id', function(req, res) {
-        let response = 'id: ' + req.params.id;
-        res.send(response);
     });
 
     app.get('/songs/:kind/:id', function(req, res) {
@@ -25,12 +28,8 @@ module.exports = function(app) {
         res.send(response);
     });
 
-    app.post('/songs/add', function (req, res){
-        let response= "Canción agregada: " + req.body.title + "<br>"
-        + " genero: " + req.body.kind + "<br>"
-        + " precio: " + req.body.price
-
-        res.send(response)
+    app.get('/songs/add', function (req, res) {
+        res.render("add.twig");
     });
 
     app.get('/promo*', function (req, res) {
