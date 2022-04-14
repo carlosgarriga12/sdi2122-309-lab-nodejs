31ac356848cc5f958aea70091b013c009b0173ae
carlosgarriga12 <carlosgarriga12@gmail.com>
2022-04-14 12:34:05 +0200

    SDI-2122-309-9.3-Mensajes y alertas.

--- musicstoreapp/routes/users.js
+++ musicstoreapp/routes/users.js
@@ -17,9 +17,11 @@ module.exports = function (app, usersRepository) {
     }
 
     usersRepository.insertUser(user).then(userId => {
-      res.redirect('/publications')
+      res.redirect("users/login" + '?message=Nuevo usuario registrado.' +
+          "&messageType=alert-info");
     }).catch(error => {
-      res.send("Error al insertar el usuario");
+      res.redirect("users/signup" + '?message=Se ha producido un error al registrar al usuario.' +
+          "&messageType=alert-danger");
     });
   });
 
@@ -41,14 +43,19 @@ module.exports = function (app, usersRepository) {
     usersRepository.findUser(filter, options).then(user => {
       if(user == null) {
         req.session.user = null;
-        res.send("Usuario no identificado")
+        res.redirect("/users/login" +
+            "?message=Email o password incorrecto"+
+            "&messageType=alert-danger ");
+
       } else {
         req.session.user = user.email;
         res.redirect("/publications")
       }
     }).catch(error => {
       req.session.user = null;
-      res.send("Se ha producido un error al buscar el usuario: " + error)
+      res.redirect("/users/login" +
+          "?message=Se ha producido un error al buscar el usuario"+
+          "&messageType=alert-danger ");
     })
   });
 
